e11c838676ed014c62bc6e0bae9c0c5537b744f4
AntonDr <31773079+AntonDr@users.noreply.github.com>
2017-10-25 23:26:57 +0300

    new

--- 18.10.17/Task2/Task2.cpp
+++ 18.10.17/Task2/Task2.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 int longe(long long int );
-int whichSquence(long long int);
-void whichPrint(long long int);
+void whichSquence(long long int,int &,int &,int &);
+void whichPrint(long long int, int, int,int);
 int main()
 {
 	long long int n;
+	int j=0, q=0,g=0;
 	cin >> n;
-	whichPrint(n);
+	whichSquence(n, j, q,g);
+	whichPrint(n,j,q,g);
 	system("pause");
 	return 0;
 
@@ -23,12 +25,12 @@ int longe(long long int n)
 	}
 	return i;
 }
-int whichSquence(long long int n)
+void whichSquence(long long int n,int &j,int &q,int &g)
 {	
-	int i = longe(n), j = 0;
-	for (int k = 1; k <= i-1; k++)
+	int i = longe(n);
+	for (int k = 1; k <= i - 1; k++)
 	{
-		if (n%10 > (n/10)%10)
+		if (n % 10 > (n / 10) % 10)
 		{
 			j++;
 		}
@@ -36,15 +38,26 @@ int whichSquence(long long int n)
 		{
 			j--;
 		}
+
+		if (n % 10 == (n / 10) % 10)
+		{
+			g++;
+		}
+		if (n % 10 == (n / 10) % 10)
+		{
+			q--;
+		}
 		n /= 10;
 	}
-	return j;
 }
-void whichPrint(long long int n)
+void whichPrint(long long int n,int j,int q,int g)
 {
-	int j = whichSquence(n);
+	
 	int i = longe(n);
-	if (j == i-1) cout << "ascending sequence";
-	if (j == -i+1) cout << "decreasing sequence";
-	if(j!=i-1 && j!=-i+1) cout << "the sequence is unordered";	
+	if ((j == i - 1) && g==0) cout << "ascending sequence";
+	else if ((j == -i + 1) && q==0) cout << "decreasing sequence";
+	else if ((j + g == i - 1) && g != 0 && j!=0) cout<< "non strictly increasing sequence";
+	else if ((j + q == -i + 1) && q != 0 && j!=0) cout << "non strictly decreasing sequence";
+	else if (((g == i - 1) && (j == 0)) && ((q == -i + 1) && j == 0)) cout << "monoton";
+	else cout << "the sequence is unordered";
 }
